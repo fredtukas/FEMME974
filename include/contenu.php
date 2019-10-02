4a89435695a4107e3da29e8c9eb90e21abf11417
kevin97426 <kevin.jams974@gmail.com>
2019-10-02 13:03:05 +0400

    update index.php & contenu.php

--- include/contenu.php
+++ include/contenu.php
@@ -1,5 +1,5 @@
 <!--carousel-->
-<div class="bd-example">
+<div class="bd-example mb-4">
       <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
           <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
@@ -8,21 +8,21 @@
         </ol>
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="https://via.placeholder.com/150x80" class="d-block w-100" alt="...">
+            <img src="./contenu.php/../images/violence1.jpg" height="450px" class="d-block w-100" alt="...">
             <div class="carousel-caption d-none d-md-block">
               <h5>First slide label</h5>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </div>
           </div>
           <div class="carousel-item">
-            <img src="https://via.placeholder.com/150x80" class="d-block w-100" alt="...">
+            <img src="./contenu.php/../images/violence2.jpg" height="450px" class="d-block w-100" alt="...">
             <div class="carousel-caption d-none d-md-block">
               <h5>Second slide label</h5>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
           <div class="carousel-item">
-            <img src="https://via.placeholder.com/150x80" class="d-block w-100" alt="...">
+            <img src="./contenu.php/../images/violence3.jpg" height="450px" class="d-block w-100" alt="...">
             <div class="carousel-caption d-none d-md-block">
               <h5>Third slide label</h5>
               <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
