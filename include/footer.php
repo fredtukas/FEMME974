f7a3c827f8b9020f4e7739a2d6bdaaa9f7c3f18b
titoff974 <fredtukas@yahoo.fr>
2019-10-02 11:22:05 +0400

    footer

--- include/footer.php
+++ include/footer.php
@@ -1,10 +1,20 @@
+
+<div class="container">
 <div class="card">
   <div class="card-header">
-    Featured
+    Footer
   </div>
   <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <h5 class="card-title"></h5>
+    <p class="card-text"></p>
+    <h5>LES TRAVAUX SUR LE BON ÉQUILIBRE DES GENRES SE FONT AVEC NOS PARTENAIRES A LA RÉUNION</h5>
+   <a href="https://www.facebook.com/pages/category/Charity-Organization/Association-AFECT-442402822969402/" target="blank"> <img src="images/1.jpg" class="logo"></a>
+    <a href="https://www.facebook.com/defisdefemmes/" target="blank"><img src="images/2.png" class="logo"></a>
+    <a href="http://finistere.cidff.info/partenaires/view/133" target="blank"><img src="images/3.png" class="logo"></a>
+    <a href="http://www.efticoi.net/efticoi-association.htm" target="blank"><img src="images/4.jpg" class="logo"></a><br><br>
+
+
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
+</div>
 </div>
