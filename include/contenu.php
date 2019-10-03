a1d5e6b45a27a954ea9697f0b14036ce0083bbe5
titoff974 <fredtukas@yahoo.fr>
2019-10-03 09:41:46 +0400

    modif contenu

--- include/contenu.php
+++ include/contenu.php
@@ -11,24 +11,24 @@
             <div class="carousel-item active">
                 <img src="./contenu.php/../images/violence1.jpg"  height="450px" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                <h5>VOUS ETES VICTIME DE VIOLENCE ?</h5>
-              <p> Qui contacter ? N'hésitez pas à appeler le 3 9 1 9</p>
+                <h5 class="carou">VOUS ETES VICTIME DE VIOLENCE ?</h5>
+              <p class="carou"> Qui contacter ? N'hésitez pas à appeler le 3 9 1 9</p>
                 </div>
             </div>
             <div class="carousel-item">
                 <!--IMAGE 2-->
                 <img src="./contenu.php/../images/violence2.jpg" height="450px" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h5 class="carou">VOUS ETES VICTIME DE MALTRAITANCE ? </h5>
+                    <p class="carou">Faites vous aider par des professionnels : NUMEROS UTILES SUR LA PLAQUETTE ORVIFF<br>(À TÉLÉCHARGER ICI)</p>
                 </div>
             </div>
             <div class="carousel-item">
                 <!--IMAGE 3-->
                 <img src="./contenu.php/../images/violence3.jpg" height="450px" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h5 class="carou">L'ASSOCIATION AFECT est notre partenaire direct pour la lutte contre les violences faites aux femmes. </h5>
+                    <p class="carou">Elle organise des groupes de parole : APPELEZ au 0692 617 659 Mme Rolande CAZAL est la présidente.</p>
                 </div>
             </div>
         </div>
@@ -46,3 +46,29 @@
 
 <!--<h5>VOUS ETES VICTIME DE VIOLENCE ?</h5>
               <p> Qui contacter ? N'hésitez pas à appeler le 3 9 1 9</p>-->
+
+              <!--collapse--> 
+
+              <p>LE SERVICE RÉGIONAL DES DROITS DES FEMMES et DE L'ÉGALITÉ DE LA RÉUNION : contacter le 0262 40 78 39 
+              situé à Saint Denis Préfecture.<br>
+            COMMUNIQUEZ SANS STÉRÉOTYPES SEXISTES : télécharger les bonnes pratiques</p>
+
+            <p>Une ASSOCIATION qui fait quoi ?</p>
+
+            <p>Femmes 974 a pour but l’étude, l’information, la défense des droits et des intérêts matériels et moraux 
+            tant collectifs qu’individuels des femmes de La Réunion et plus généralement la défense et la promotion des droits des femmes.</p>
+
+  <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+    Lire la suite
+  </a>
+  <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    Button with data-target
+  </button>
+</p>
+<div class="collapse" id="collapseExample">
+  <div class="card card-body">
+  Sa mission est d'analyser les causes du harcèlement, des injustices, des violences envers les femmes afin d'élaborer des outils, 
+  de mettre en place des actions, de rassembler les solidarités, d'encourager le changement des mentalités, d'enclencher le respect et 
+  d'encourager les femmes à être ambitieuses et leaders dans tous les domaines de la vie privée, professionnelle et publique.
+  </div>
+</div>
