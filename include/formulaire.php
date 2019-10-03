ac1b5b0c539b343b9d2672f6c8cb655ca11111e6
kevin97426 <kevin.jams974@gmail.com>
2019-10-03 11:59:00 +0400

    modifier elements in formulaire.php

--- include/formulaire.php
+++ include/formulaire.php
@@ -1,7 +1,6 @@
+<h2 class="text-center m-4">Contact</h2>
 
-
-<div class="container">
-<form>
+<form class="m-4">
   <div class="row">
     <div class="col">
       <input type="text" class="form-control" placeholder="Nom">
@@ -10,18 +9,16 @@
       <input type="text" class="form-control" placeholder="Prenom">
     </div>
   </div>
-</form>
-
-<form>
   <div class="form-group">
     <label for="exampleFormControlInput1"></label>
     <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nom@exemple.com">
   </div>
- 
+
   <div class="form-group">
     <label for="exampleFormControlTextarea1"></label>
     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Votre message"></textarea>
   </div>
+
+  <button type="button" class="btn btn-primary">Envoyer</button>
 </form>
-<button type="button" class="btn btn-primary">Envoyer</button>
-</div>
+
