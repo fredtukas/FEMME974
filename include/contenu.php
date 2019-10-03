89d916fbfb697f8667d59e5da6a3215e04f42589
titoff974 <fredtukas@yahoo.fr>
2019-10-03 11:00:37 +0400

    contenu

--- include/contenu.php
+++ include/contenu.php
@@ -1,3 +1,5 @@
+
+
 <!--carousel-->
 <div class="bd-example mb-4">
     <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
@@ -11,24 +13,25 @@
             <div class="carousel-item active">
                 <img src="./contenu.php/../images/violence1.jpg"  height="450px" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                <h5 class="carou">VOUS ETES VICTIME DE VIOLENCE ?</h5>
-              <p class="carou"> Qui contacter ? N'hésitez pas à appeler le 3 9 1 9</p>
+                <h5 class="carou"><strong>VOUS ETES VICTIME DE VIOLENCE ?</strong></h5>
+              <p class="carou"><strong> Qui contacter ? N'hésitez pas à appeler le </strong><h4>3 9 1 9</h4></p>
                 </div>
             </div>
             <div class="carousel-item">
                 <!--IMAGE 2-->
                 <img src="./contenu.php/../images/violence2.jpg" height="450px" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5 class="carou">VOUS ETES VICTIME DE MALTRAITANCE ? </h5>
-                    <p class="carou">Faites vous aider par des professionnels : NUMEROS UTILES SUR LA PLAQUETTE ORVIFF<br>(À TÉLÉCHARGER ICI)</p>
+                    <h5 class="carou"><strong>VOUS ETES VICTIME DE MALTRAITANCE ? </strong></h5>
+                    <p class="carou"><strong>Faites vous aider par des professionnels : NUMEROS UTILES SUR LA PLAQUETTE ORVIFF<br>
+                    <a href="http://www.femmes974.info/plaquette-ARAJUFA-01-08-19.pdf">(À TÉLÉCHARGER ICI)</strong></a></p>
                 </div>
             </div>
             <div class="carousel-item">
                 <!--IMAGE 3-->
                 <img src="./contenu.php/../images/violence3.jpg" height="450px" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5 class="carou">L'ASSOCIATION AFECT est notre partenaire direct pour la lutte contre les violences faites aux femmes. </h5>
-                    <p class="carou">Elle organise des groupes de parole : APPELEZ au 0692 617 659 Mme Rolande CAZAL est la présidente.</p>
+                    <h5 class="carou"><strong>L'ASSOCIATION AFECT est notre partenaire direct pour la lutte contre les violences faites aux femmes. </strong> </h5>
+                    <p class="carou"><strong>Elle organise des groupes de parole : APPELEZ au 0692 617 659 Mme Rolande CAZAL est la présidente. </strong></p>
                 </div>
             </div>
         </div>
@@ -49,21 +52,19 @@
 
               <!--collapse--> 
 
-              <p>LE SERVICE RÉGIONAL DES DROITS DES FEMMES et DE L'ÉGALITÉ DE LA RÉUNION : contacter le 0262 40 78 39 
+              <p class="text-center">LE SERVICE RÉGIONAL DES DROITS DES FEMMES et DE L'ÉGALITÉ DE LA RÉUNION : contacter le 0262 40 78 39 
               situé à Saint Denis Préfecture.<br>
             COMMUNIQUEZ SANS STÉRÉOTYPES SEXISTES : télécharger les bonnes pratiques</p>
 
-            <p>Une ASSOCIATION qui fait quoi ?</p>
+            <p class="text-center">Une ASSOCIATION qui fait quoi ?</p>
 
-            <p>Femmes 974 a pour but l’étude, l’information, la défense des droits et des intérêts matériels et moraux 
+            <p class="text-center">Femmes 974 a pour but l’étude, l’information, la défense des droits et des intérêts matériels et moraux 
             tant collectifs qu’individuels des femmes de La Réunion et plus généralement la défense et la promotion des droits des femmes.</p>
 
-  <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+  <a class="btn btn-fred" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
     Lire la suite
   </a>
-  <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-    Button with data-target
-  </button>
+
 </p>
 <div class="collapse" id="collapseExample">
   <div class="card card-body">
@@ -71,4 +72,17 @@
   de mettre en place des actions, de rassembler les solidarités, d'encourager le changement des mentalités, d'enclencher le respect et 
   d'encourager les femmes à être ambitieuses et leaders dans tous les domaines de la vie privée, professionnelle et publique.
   </div>
+ 
 </div>
+<h5 class="text-center">COMMENT ?</h5><br>
+
+<ul class="text-center">
+<ol><strong>RENFORCER LES DISPOSITIFS EXISTANTS SUR LES DROITS DES FEMMES :</strong> sur le plan de l'application des lois sur le plan professionnel 
+et familail et dans son ensemble sur le respect des droits.<br><br>
+</ol>
+<ol><strong>ACTIVER LES RÉSEAUX AVEC LES DIFFERENTS ACTEURS :</strong> créer des passerelles entre institutions, associations, mutualiser les moyens 
+dans le but d'un centre de ressources et de donner du sens à nos combats.<br><br>
+</ol>
+<ol><strong>FORMER ET INFORMER A TOUTES LES THÉMATIQUES LIÉES AUX DROITS DES FEMMES :</strong> intervenants, partenaires, presse, élus, tout public.<br><br></ol>
+</ul>
+
