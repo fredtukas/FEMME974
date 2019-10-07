a872e848fdb78f23ad85c10babb4d4365f355e5f
kevin97426 <kevin.jams974@gmail.com>
2019-10-07 11:57:22 +0400

    add modification propositions.php & liste-asso.php

--- include/formulaire.php
+++ include/formulaire.php
@@ -1,22 +1,29 @@
-<form class="bg-blanc">
-  <h2 class="text-center" style="color:#c10000;">Contact</h2>
+<div class="bg-blanc">
+
+<h2 class="text-center" style="color:#c10000;">Contact</h2>
+
+<form action="send.php" method="post">
+  
   <div class="row">
     <div class="col m-2">
-      <input type="text" class="form-control" placeholder="Nom">
+      <input type="text" class="form-control" name="nom" placeholder="Nom">
     </div>
     <div class="col m-2">
-      <input type="text" class="form-control" placeholder="Prenom">
+      <input type="text" class="form-control" name="prenom" placeholder="Prenom">
     </div>
   </div>
   <div class="form-group m-2">
-    <label for="exampleFormControlInput1"></label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nom@exemple.com">
+    <label for="exampleInputEmail1"></label>
+    <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder="Votre email">
   </div>
 
   <div class="form-group m-2">
     <label for="exampleFormControlTextarea1"></label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Votre message"></textarea>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="message" placeholder="Votre message"></textarea>
   </div>
 
-  <p class="text-center"><button type="button" class="btn btn-red">Envoyer</button></p>
+  <p class="text-center"><button type="submit" name="submit" class="btn btn-red">Envoyer</button></p>
 </form>
+
+</div>
+
